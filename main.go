6421390e0a1e2713e0dfe2e407282379f4dcb241
yantun.wang <yantun.wang@mihoyo.com>
2021-09-23 14:39:09 +0800

    launch

--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	jwt "github.com/form3tech-oss/jwt-go"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
+
+const (
+	AUTH0_API_CLIENT_SECRET = `E5ndDj5dyzt8ITxySi41Bhc-tkfxCdZagOzS8fHvyxpys1pGLjOhohpQqU7qGD0M`
+	AUTH0_CLIENT_ID         = `CAc8adGXJDu0CmOxIZtRJKm4ffBTEXSE`
+	AUTH0_DOMAIN            = `dev-ukkow86e.us.auth0.com`
+	AUTH0_API_AUDIENCE      = `http://jokeish-app`
+)
+
+type Jwks struct {
+	Keys []JSONWebKeys `json:"keys"`
+}
+
+type JSONWebKeys struct {
+	Kty string   `json:"kty"`
+	Kid string   `json:"kid"`
+	Use string   `json:"use"`
+	N   string   `json:"n"`
+	E   string   `json:"e"`
+	X5c []string `json:"x5c"`
+}
 
 type Joke struct {
 	ID    int    `json:"id" binding:"required"`
@@ -24,7 +50,16 @@ var jokes = []Joke{
 	{7, 0, "How does a penguin build it's house? Igloos it together."},
 }
 
+var jwtMiddleWare *jwtmiddleware.JWTMiddleware
+
 func main() {
+	var getterFunc jwt.Keyfunc
+	getterFunc = validationKeyGetterFunc
+	jwtMiddleWare = jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: getterFunc,
+		SigningMethod:       jwt.SigningMethodRS256,
+	})
+
 	router := gin.Default()
 	router.Use(static.Serve("/", static.LocalFile("./views", true)))
 	api := router.Group("/api")
@@ -35,10 +70,76 @@ func main() {
 			})
 		})
 	}
-	api.GET("/jokes", JokeHandler)
-	api.POST("/jokes/like/:jokeID", LikeJoke)
+	api.GET("/jokes", authMiddleware(), JokeHandler)
+	api.POST("/jokes/like/:jokeID", authMiddleware(), LikeJoke)
 
 	router.Run(":3000")
+}
+
+func validationKeyGetterFunc(token *jwt.Token) (interface{}, error) {
+	aud := AUTH0_API_AUDIENCE
+	checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+	if !checkAudience {
+		return token, errors.New("Invalid audience.")
+	}
+	// verify iss claim
+	iss := AUTH0_DOMAIN
+	checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+	if !checkIss {
+		return token, errors.New("Invalid issuer.")
+	}
+
+	cert, err := getPemCert(token)
+	if err != nil {
+		log.Fatalf("could not get cert: %+v", err)
+	}
+
+	result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
+	return result, nil
+}
+
+func authMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Get the client secret key
+		err := jwtMiddleWare.CheckJWT(c.Writer, c.Request)
+		if err != nil {
+			// Token not found
+			fmt.Println(err)
+			c.Abort()
+			c.Writer.WriteHeader(http.StatusUnauthorized)
+			c.Writer.Write([]byte("Unauthorized"))
+			return
+		}
+	}
+}
+
+func getPemCert(token *jwt.Token) (string, error) {
+	cert := ""
+	resp, err := http.Get(AUTH0_DOMAIN + ".well-known/jwks.json")
+	if err != nil {
+		return cert, err
+	}
+	defer resp.Body.Close()
+
+	var jwks = Jwks{}
+	err = json.NewDecoder(resp.Body).Decode(&jwks)
+
+	if err != nil {
+		return cert, err
+	}
+
+	x5c := jwks.Keys[0].X5c
+	for k, v := range x5c {
+		if token.Header["kid"] == jwks.Keys[k].Kid {
+			cert = "-----BEGIN CERTIFICATE-----\n" + v + "\n-----END CERTIFICATE-----"
+		}
+	}
+
+	if cert == "" {
+		return cert, errors.New("unable to find appropriate key.")
+	}
+
+	return cert, nil
 }
 
 func JokeHandler(c *gin.Context) {
